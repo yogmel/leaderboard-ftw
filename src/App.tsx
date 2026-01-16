09ecc3517853e3f7eefd50ce2ceadcb4f624d465
Mellina Yonashiro <yonashiro.mellina@gmail.com>
2026-01-16 21:35:32 +0100

    change style and add loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import "./App.css";
-import { Heading, Highlight } from "@chakra-ui/react";
+import { Heading, Highlight, Skeleton } from "@chakra-ui/react";
 import TotalDistanceCard from "./components/TotalDistanceCard";
 import { useActivities } from "./hooks/useActivities";
 import CurrentLeaderCard from "./components/CurrentLeaderCard";
 import styles from "./styles/typography.module.css";
 
 function App() {
-  const { totalOne, totalTwo } = useActivities();
+  const { totalOne, totalTwo, isLoading } = useActivities();
 
   return (
     <>
@@ -16,9 +16,23 @@ function App() {
         </Highlight>
       </Heading>
 
-      <CurrentLeaderCard totalOne={totalOne} totalTwo={totalTwo} />
+      {isLoading ? (
+        <Skeleton
+          variant="shine"
+          width="full"
+          height="20"
+          css={{
+            "--start-color": "colors.pink.500",
+            "--end-color": "colors.orange.500",
+          }}
+        />
+      ) : (
+        <>
+          <CurrentLeaderCard totalOne={totalOne} totalTwo={totalTwo} />
 
-      <TotalDistanceCard totalOne={totalOne} totalTwo={totalTwo} />
+          <TotalDistanceCard totalOne={totalOne} totalTwo={totalTwo} />
+        </>
+      )}
     </>
   );
 }
--- src/hooks/useActivities.ts
+++ src/hooks/useActivities.ts
@@ -4,24 +4,27 @@ import { getApiUrl } from "../api/config";
 import { getTotalFromActivities } from "../services/processActivities";
 
 export const useActivities = () => {
-  const [totalOne, setTotalOne] = useState<number>(0); // red panda points
-  const [totalTwo, setTotalTwo] = useState<number>(0); // growling monkey points
+  const [totalOne, setTotalOne] = useState<number>(0);
+  const [totalTwo, setTotalTwo] = useState<number>(0);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const fetchActivities = async () => {
     const data = await axios.get(getApiUrl("activities"));
-
     const totalOne = getTotalFromActivities(data.data.dataOne);
     const totalTwo = getTotalFromActivities(data.data.dataTwo);
-
     return { totalOne, totalTwo };
   };
 
   useEffect(() => {
-    fetchActivities().then((data) => {
-      setTotalOne(data.totalOne);
-      setTotalTwo(data.totalTwo);
-    });
+    fetchActivities()
+      .then((data) => {
+        setTotalOne(data.totalOne);
+        setTotalTwo(data.totalTwo);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }, []);
 
-  return { totalOne, totalTwo };
+  return { totalOne, totalTwo, isLoading };
 };
