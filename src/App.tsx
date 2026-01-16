b3923283ec514afdd03ee3884d9c4b2b659fc5cc
Mellina Yonashiro <yonashiro.mellina@gmail.com>
2026-01-16 21:15:49 +0100

    add current leader

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import "./App.css";
-import { Heading } from "@chakra-ui/react";
+import { Heading, Highlight } from "@chakra-ui/react";
 import TotalDistanceCard from "./components/TotalDistanceCard";
 import { useActivities } from "./hooks/useActivities";
+import CurrentLeaderCard from "./components/CurrentLeaderCard";
+import styles from "./styles/typography.module.css";
 
 function App() {
   const { totalOne, totalTwo } = useActivities();
 
   return (
     <>
-      <Heading>Leaderboard for running since 11/01/2026</Heading>
+      <Heading size={"6xl"} className={styles.title}>
+        <Highlight query="for the win" styles={{ color: "#C3423F" }}>
+          Leaderboard for the win
+        </Highlight>
+      </Heading>
+
+      <CurrentLeaderCard totalOne={totalOne} totalTwo={totalTwo} />
+
       <TotalDistanceCard totalOne={totalOne} totalTwo={totalTwo} />
     </>
   );
---
+++ src/components/CurrentLeaderCard.tsx
@@ -0,0 +1,37 @@
+import { Flex, Heading, Image } from "@chakra-ui/react";
+import BugioImg from "./../assets/growling-monkey.png";
+import PandaImg from "./../assets/red-panda.png";
+import styles from "./../styles/typography.module.css";
+import currentLeaderCardStyles from "./currentLeaderCard.module.css";
+import { truncateTwoDecimals } from "../utils/numberUtils";
+
+interface TotalDistanceCardProps {
+  totalOne: number;
+  totalTwo: number;
+}
+
+function CurrentLeaderCard({ totalOne, totalTwo }: TotalDistanceCardProps) {
+  const isOneWinner = totalOne > totalTwo;
+  const difference = isOneWinner ? totalOne - totalTwo : totalTwo - totalOne;
+  const imageSrc = isOneWinner ? PandaImg : BugioImg;
+  const winnerText = isOneWinner ? "Panda Vermelho" : "Macaco Bugio";
+
+  return (
+    <Flex style={{ top: "-70px" }}>
+      <Image src={imageSrc} style={{ width: "100%", maxWidth: "600px" }} />
+      <div className={currentLeaderCardStyles.card}>
+        <Heading size={"6xl"} className={styles.currentLeaderTitle}>
+          Current Leader
+        </Heading>
+        <Heading size={"6xl"} className={styles.currentLeaderSubtitle}>
+          {winnerText}
+        </Heading>
+        <Heading size={"2xl"}>
+          + {truncateTwoDecimals(difference / 1000)} km
+        </Heading>
+      </div>
+    </Flex>
+  );
+}
+
+export default CurrentLeaderCard;
