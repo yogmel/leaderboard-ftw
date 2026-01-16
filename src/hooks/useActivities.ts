09d88b30349f00fbaa3ca6cea7e5cfb8696cf530
Mellina Yonashiro <yonashiro.mellina@gmail.com>
2026-01-16 23:07:01 +0100

    add cached mode for less api calls during development

--- src/hooks/useActivities.ts
+++ src/hooks/useActivities.ts
@@ -3,10 +3,18 @@ import { useState, useEffect } from "react";
 import { getApiUrl } from "../api/config";
 import { getTotalFromActivities } from "../services/processActivities";
 
+const isCached = import.meta.env.MODE === "cached";
+const defaultTotalOne = 23000;
+const defaultTotalTwo = 22000;
+
 export const useActivities = () => {
-  const [totalOne, setTotalOne] = useState<number>(0);
-  const [totalTwo, setTotalTwo] = useState<number>(0);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [totalOne, setTotalOne] = useState<number>(
+    isCached ? defaultTotalOne : 0
+  );
+  const [totalTwo, setTotalTwo] = useState<number>(
+    isCached ? defaultTotalTwo : 0
+  );
+  const [isLoading, setIsLoading] = useState<boolean>(!isCached);
 
   const fetchActivities = async () => {
     const data = await axios.get(getApiUrl("activities"));
@@ -16,14 +24,16 @@ export const useActivities = () => {
   };
 
   useEffect(() => {
-    fetchActivities()
-      .then((data) => {
-        setTotalOne(data.totalOne);
-        setTotalTwo(data.totalTwo);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
+    if (!isCached) {
+      fetchActivities()
+        .then((data) => {
+          setTotalOne(data.totalOne);
+          setTotalTwo(data.totalTwo);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    }
   }, []);
 
   return { totalOne, totalTwo, isLoading };
